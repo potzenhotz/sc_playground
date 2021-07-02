045b61b5a7c259214eaab0a7162e8909d993d209
potzenhotz <lukasmuessle@gmail.com>
2021-07-02 23:06:56 +0200

    intital setup

---
+++ src/main/scala/example/Hello.scala
@@ -0,0 +1,24 @@
+package example
+
+object Hello extends Greeting with App {
+  println(greeting)
+
+  val curry1 = testCurry1("test")("test2")
+  val curry2 = testCurry2("test")("test2")
+
+  println(curry1)
+  println(curry2)
+
+  def testCurry1(input: String): String => String = { input2 =>
+    s"${input} and ${input2}"
+  }
+
+  def testCurry2(input: String)(input2: String): String = {
+    s"${input} and ${input2}"
+  }
+
+}
+
+trait Greeting {
+  lazy val greeting: String = "hello"
+}
